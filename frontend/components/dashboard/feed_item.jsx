0f0fca9271fe136765d970459fe027c4e0b83041
your name here <jennhuynh02@gmail.com>
2020-05-04 01:33:02 -0700

    Rendered answers edit and delete buttons

--- frontend/components/dashboard/feed_item.jsx
+++ frontend/components/dashboard/feed_item.jsx
@@ -1,12 +1,36 @@
 import React from 'react';
 
-const FeedItem = ({ question, deleteQuestion, openModal }) => (
+const FeedItem = ({ question, deleteQuestion, openModal, answers }) => {
+
+  let a = null;
+  let answerId = null;
+  for (let i = 0; i < answers.length; i++) {
+    if (question.id === answers[i].question_id) {
+      a = answers[i].answer;
+      answerId = answers[i].id;
+      break;
+    }
+  }
+
+
+
+  return (
+
   <div className="feed-item">
-    { question.ask }
+    <p className="feed-question">{ question.ask }</p>
     <button onClick={() => openModal({ 'editQuestion': question.id })}>Edit Question</button>
     <button onClick={() => deleteQuestion(question.id)}>Delete Question</button>
+
+    <br/>
+    { a }
+    <br/>
+
+    <button onClick={() => openModal({ 'editAnswer': answerId })}>Edit Answer</button>
+    <button onClick={() => deleteAnswer(answerId)}>Delete Answer</button>    
+
   </div>
-);
+  );
+};
 
 
 export default FeedItem;
