b8369a5e255f7f060c54fcfb3c9c043e9cdc5ee4
your name here <jennhuynh02@gmail.com>
2020-05-05 14:44:46 -0700

    Added dropdown to header for profile logout

--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -3,8 +3,13 @@ import React from 'react';
 const Header = (props) => (
   <div id="header">
     <h1 id="logo">Question Aura</h1>
-    <p className="dropdown">Drop</p>
-    <button onClick={() => props.logout()}>Logout</button>
+    <ul className="dropdown">
+        <img className="profile-dropdown" src={window.profilePic} />
+        <li className="dropdown-content">
+
+        <p onClick={() => props.logout()}>Logout</p>
+        </li>
+    </ul>
     <button type="submit" className="header-question-click" onClick={() => props.openModal({'question': -1})}>Ask Question</button>
   </div>
 
