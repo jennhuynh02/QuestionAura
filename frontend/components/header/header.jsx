1f55298b8529463699bfac4bedd5bf84bb236abc
your name here <jennhuynh02@gmail.com>
2020-05-05 13:35:34 -0700

    Added spacing height for feed item components

--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const Header = (props) => (
   <div id="header">
     <h1 id="logo">Question Aura</h1>
+    <p className="dropdown">Drop</p>
     <button onClick={() => props.logout()}>Logout</button>
     <button type="submit" className="header-question-click" onClick={() => props.openModal({'question': -1})}>Ask Question</button>
   </div>
