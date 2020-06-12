6edb256810de1d76169453e512a78daeeb86574d
your name here <jennhuynh02@gmail.com>
2020-06-12 15:43:27 -0700

    Editing searchbar so that it doesn't display results on empty search

--- frontend/components/searchbar/searchbar.jsx
+++ frontend/components/searchbar/searchbar.jsx
@@ -21,29 +21,36 @@ class SearchBar extends React.Component {
   search() {
     this.searchResults = [];
     const { questions, topics } = this.props;
-    let topicList = {};
+    const topicList = {};
     if (topics.length > 0) {
       for (let i = 0; i < topics.length; i++) {
-        topicList[topics[i].id] = topics[i].name
+        topicList[topics[i].id] = topics[i].name;
       }
     }
-    for (let i = 0; i < questions.length; i += 1) {
-      if (questions[i].ask.includes(this.state.searchInput) && this.searchResults.length !== 5) {
-        this.searchResults.push(questions[i]);
+    if (this.state.searchInput.length !== 0) {
+      for (let i = 0; i < questions.length; i += 1) {
+        if (questions[i].ask.includes(this.state.searchInput) && this.searchResults.length !== 5) {
+          this.searchResults.push(questions[i]);
+        }
       }
     }
     if (this.searchResults.length > 0) {
       return (
         <ul className="dropdown-content">
           {this.searchResults.map((question) => (
-            <li onClick={(e) => {e.preventDefault();
-              location.href = `/#/questions/${question.id}`}} 
-              key={question.id} className="searchbar-result" >
-                {/* <span className="topic-search">Topic:  </span> */}
-                <img className="search-img" src={window[topicList[question.topic_id]]} />
-                {/* {topicList[question.topic_id]} */}
-                {question.ask}
-                </li>
+            <li
+              onClick={(e) => {
+                e.preventDefault();
+                location.href = `/#/questions/${question.id}`;
+              }}
+              key={question.id}
+              className="searchbar-result"
+            >
+              {/* <span className="topic-search">Topic:  </span> */}
+              <img className="search-img" src={window[topicList[question.topic_id]]} />
+              {/* {topicList[question.topic_id]} */}
+              {question.ask}
+            </li>
           ))}
         </ul>
       );
@@ -56,16 +63,16 @@ class SearchBar extends React.Component {
         <form>
           <div className="searchbar">
             <img className="mag-glass" src={window.magnifyingGlass} />
-          <input
-            type="text"
-            placeholder="Search Question Aura"
-            value={this.state.searchInput}
-            onChange={this.update('searchInput')}
-            className="search-input"
+            <input
+              type="text"
+              placeholder="Search Question Aura"
+              value={this.state.searchInput}
+              onChange={this.update('searchInput')}
+              className="search-input"
             />
           </div>
         </form>
-          {this.search()}
+        {this.search()}
       </div>
     );
   }
